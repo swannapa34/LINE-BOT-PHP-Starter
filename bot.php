941771e8310e5737e862455162c82f8c4b87bcc4
swannapa34 <36792838+swannapa34@users.noreply.github.com>
2018-02-25 12:16:05 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,7 +14,7 @@ if (!is_null($events['events'])) {
 		// Build message to reply back
 		$messages = [
 			'type' => 'text',
-			'text' => $event['message']['type'] 
+			'text' => json_encode($event['message']['type']) 
 		];
 		// Make a POST Request to Messaging API to reply to sender
 		$url = 'https://api.line.me/v2/bot/message/reply';

ee1082bc63b5b1a1c8ca3e7542ea97758b0e02e4
swannapa34 <36792838+swannapa34@users.noreply.github.com>
2018-02-25 10:20:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,39 +9,32 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+		// Get replyToken
+		$replyToken = $event['replyToken'];
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+		// Build message to reply back
+		$messages = $event['message'];
+		$data = [
+			'replyToken' => $replyToken,
+			'messages' => [$messages],
+		];
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+		// Make a POST Request to Messaging API to reply to sender
+		$url = 'https://api.line.me/v2/bot/message/reply';			
+		$post = json_encode($data);
+		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+		$channel = curl_init();
+		curl_setopt($channel, CURLOPT_URL, $url);
+		curl_setopt($channel, CURLOPT_POST, true);
+		curl_setopt($channel, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($channel, CURLOPT_POSTFIELDS, $post);
+		curl_setopt($channel, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($channel, CURLOPT_FOLLOWLOCATION, 1);
+		$result = curl_exec($channel);
+		curl_close ($channel);
 
-			echo $result . "\r\n";
-		}
+		echo $result . "\r\n";
 	}
 }
 echo "OK";

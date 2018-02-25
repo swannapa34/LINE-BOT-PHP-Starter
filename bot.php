61544ac4fec0d154b5552fb5fb7bb17be9fbd028
swannapa34 <36792838+swannapa34@users.noreply.github.com>
2018-02-25 10:44:47 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -11,28 +11,29 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Get replyToken
 		$replyToken = $event['replyToken'];
-
 		// Build message to reply back
-		$messages = $event['message'];
+		$messages = [
+			'type' => 'text',
+			'text' => $event['message']['type'] 
+		];
+		// Make a POST Request to Messaging API to reply to sender
+		$url = 'https://api.line.me/v2/bot/message/reply';
 		$data = [
 			'replyToken' => $replyToken,
-			'messages' => $event['message']['type'],
+			'messages' => [$messages],
 		];
 
 		// Make a POST Request to Messaging API to reply to sender
-		$url = 'https://api.line.me/v2/bot/message/reply';			
 		$post = json_encode($data);
 		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-		$channel = curl_init();
-		curl_setopt($channel, CURLOPT_URL, $url);
-		curl_setopt($channel, CURLOPT_POST, true);
-		curl_setopt($channel, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($channel, CURLOPT_POSTFIELDS, $post);
-		curl_setopt($channel, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($channel, CURLOPT_FOLLOWLOCATION, 1);
-		$result = curl_exec($channel);
-		curl_close ($channel);
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		$result = curl_exec($ch);
+		curl_close($ch);
 
 		echo $result . "\r\n";
 	}
